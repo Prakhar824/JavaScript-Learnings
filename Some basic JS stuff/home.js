c9475b0af1713bf799e0e9ede4449cc6a420e02d
Agarwal <prakhar.agarwal@appliedis.com>
2021-04-20 00:14:55 +0530

    Some basic JS stuff

---
+++ Some basic JS stuff/home.js
@@ -0,0 +1,13 @@
+console.log('Hello from the js file.');
+var b = "Hello, I am a variable";
+console.log(b);
+// manipulating dom using JS.
+document.getElementById('someText').innerHTML = 'Hello';
+
+// functions in JS.
+
+function fun(){
+    alert('Hello from function');
+}
+
+fun();
